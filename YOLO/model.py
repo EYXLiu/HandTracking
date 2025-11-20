7a2d6c4b05626c9d75cc4037bafa44afe0054e96
Evan Liu <144865565+EYXLiu@users.noreply.github.com>
2025-11-19 23:25:55 -0500

    Fixed small typos

--- YOLO/model.py
+++ YOLO/model.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
-import torch.functional as F 
+import torch.nn.functional as F 
 
-class YOLO(nn.Modele):
+class YOLO(nn.Module):
     def __init__(self, num_classes, S, B):
         super (YOLO, self).__init__()
         self.num_classes = num_classes
@@ -28,11 +28,11 @@ class YOLO(nn.Modele):
     def forward(self, x):
         x = self.cnn(x)
         x = self.detect(x)
-        x = x.view(-1, self.S, self.S, self.B * 5 + self.C)
+        x = x.view(-1, self.S, self.S, self.B * 5 + self.num_classes)
         return x
     
     def predict(self, x):
-        x = self.cnn(x), 
+        x = self.cnn(x) 
         x = self.detect(x)
         x = x.view(-1, self.S, self.S, self.B * 5 + self.C)
         x = x.sigmoid()
@@ -45,10 +45,10 @@ def YOLO_loss(predictions, targets, S, B, num_classes):
     obj_mask = targets[...,4] > 0
     noobj_mask = ~obj_mask
     
-    coord_loss = lambda_coord * torch.sum((predictions[...,:2] - targets[...,:2]) ** 2) # MSE for bounding box
+    coord_loss = lambda_coord * torch.sum((predictions[...,:4] - targets[...,:4]) ** 2) # MSE for bounding box
     obj_loss = torch.sum((predictions[obj_mask, 4] - targets[obj_mask, 4]) ** 2) # MSE for confidence of object
     noobj_loss = lambda_noobj * torch.sum((predictions[noobj_mask, 4] - targets[noobj_mask, 4]) ** 2) # MSE if theres no object
-    class_loss = F.cross_entropy(predictions[...,5:], targets[...,5:])
+    class_loss = F.mse_loss(predictions[...,5:], targets[...,5:])
     
     return coord_loss + obj_loss + noobj_loss + class_loss
     
